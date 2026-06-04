e0c802cae2917ff14e7c7aff34c151907b0250dd
aliciagomezaviles <gomez.cia@hotmail.com>
2026-06-04 16:28:54 +0200

    Conversiones de tipo primitivos a cadenas y entre tipos primitivos

--- ejercicios-java/src/ConversionDeTipos.java
+++ ejercicios-java/src/ConversionDeTipos.java
@@ -1,5 +1,8 @@
 public class ConversionDeTipos {
     public static void main(String[] args) {
+
+        //Conversión de cadenas a primitivos
+
         String numeroStr = "50";
 
         int numeroInt =Integer.parseInt(numeroStr);
@@ -12,5 +15,36 @@ public class ConversionDeTipos {
         String logicoStr = "true"; //false
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
+
+        int otroNumeroInt = 100;
+
+        System.out.println("otroNumeroInt = " + otroNumeroInt);
+
+        String otroNumeroStr = Integer.toString(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        otroNumeroStr = String.valueOf(otroNumeroInt);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        otroNumeroStr = String.valueOf(otroNumeroInt+10);
+        System.out.println("otroNumeroStr = " + otroNumeroStr);
+
+        //Conversión de tipos primitivos a cadena
+
+        double otroRealDouble = 1.23456;
+        String otroRealStr = Double.toString(otroRealDouble);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        otroRealStr = String.valueOf(1.23456f);
+        System.out.println("otroRealStr = " + otroRealStr);
+
+        //Conversión de primitivos a primitivos
+
+        int i = 10000;
+        short s = (short)i;
+        System.out.println("s = " + s);
+        long l = i;
+        System.out.println("l = " + l);
+        
     }
 }
